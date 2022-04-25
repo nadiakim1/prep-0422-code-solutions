3e49e569ad464477a0369fc7c874feadc4fa1bc0
Nadia Kim <nadia.yj14@gmail.com>
2022-04-25 00:58:47 +0000

    Completed prep-javascript-objects exercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,9 @@
+var person = { firstName: 'Carl', lastName: 'Garth', hobby: 'running' };
+console.log(person);
+var fullName = 'Carl Garth';
+console.log("The person's name is:" + ' ' + fullName + '.');
+person.job = 'chef';
+console.log("The person's job is:", person.job + '.');
+person.previousJob = 'janitor';
+console.log("The person's previous job is:", person.previousJob + '.');
+console.log(person);
